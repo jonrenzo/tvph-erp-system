2712d31bc0c07f5985d4adfbf00380117f77a231
Renzo Toledo <jonrenzotoledo0303@gmail.com>
2026-07-17 12:58:36 +0800

    fix: default to light theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col font-sans">
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
           themes={["light", "dark", "midnight"]}
